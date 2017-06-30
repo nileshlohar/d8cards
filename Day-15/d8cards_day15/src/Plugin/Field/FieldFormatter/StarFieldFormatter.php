6b872cc65f201b1b9857f010ba7ec36c1eb67df7
Nilesh Lohar <nlohar@gmail.com>
2017-06-30 19:37:47 +0530

    Day 15 ­ Creating a Custom Field Formatter

---
+++ Day-15/d8cards_day15/src/Plugin/Field/FieldFormatter/StarFieldFormatter.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Drupal\d8cards_day15\Plugin\Field\FieldFormatter;
+
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Field\FieldItemInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Plugin implementation of the 'star_field_formatter' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "star_field_formatter",
+ *   label = @Translation("Star field formatter"),
+ *   field_types = {
+ *     "decimal"
+ *   }
+ * )
+ */
+class StarFieldFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return [
+      // Implement default settings.
+    ] + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    return [
+      // Implement settings form.
+    ] + parent::settingsForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = [];
+    // Implement settings summary.
+
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+
+    foreach ($items as $delta => $item) {
+      $percentage = $this->viewValue($item) * 20;
+      $elements[$delta] = [
+          '#theme' => 'star_field_formatter',
+          '#percentage' => $percentage,
+          '#attached' => array('library'=> array('d8cards_day15/star_field_formatter_library')),
+
+      ];
+    }
+
+    return $elements;
+  }
+
+  /**
+   * Generate the output appropriate for one field item.
+   *
+   * @param \Drupal\Core\Field\FieldItemInterface $item
+   *   One field item.
+   *
+   * @return string
+   *   The textual output generated.
+   */
+  protected function viewValue(FieldItemInterface $item) {
+    // The text value has no text format assigned to it, so the user input
+    // should equal the output, including newlines.
+    return nl2br(Html::escape($item->value));
+  }
+
+}
